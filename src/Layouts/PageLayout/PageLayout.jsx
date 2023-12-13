cef37a88dfa405eb3fdd8f7c6147822c5e5bbf43
Kriso068 <nicolas.gutknecht@numericable.fr>
2023-12-13 16:53:51 +0100

    mise en place de firestore, register

--- src/Layouts/PageLayout/PageLayout.jsx
+++ src/Layouts/PageLayout/PageLayout.jsx
@@ -1,4 +1,4 @@
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, Spinner } from '@chakra-ui/react';
 import Sidebar from '../../components/Sidebar/Sidebar';
 import { useLocation } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
@@ -8,12 +8,15 @@ import Navbar from '../../components/Navbar/Navbar';
 const PageLayout = ({children}) => {
 
     const {pathname} = useLocation();
-    const [user, loading, error] = useAuthState(auth);
+    const [user, loading] = useAuthState(auth);
     const canRenderSidebar = pathname !== '/auth' && user;
     const canRenderNavbar = !user && !loading && pathname !== '/auth';
 
+    const checkingUserIsAuth = !user && loading;
+    if(checkingUserIsAuth) return <PageLayoutSpinner />;
+
   return (
-    <Flex>
+    <Flex flexDir={canRenderNavbar ? "column" : "row"}>
         {/* sidebar on the left */}
         {canRenderSidebar ? (
             <Box w={{base:"70px", md:"240px"}}>
@@ -23,7 +26,7 @@ const PageLayout = ({children}) => {
         {/* Navbar */}
         {canRenderNavbar ? <Navbar /> : null}
         {/* page content on the right */}
-        <Box flex={1} w={{base:"calc(100% - 70px)", md:"calc(100% - 240px)"}}>
+        <Box flex={1} w={{base:"calc(100% - 70px)", md:"calc(100% - 240px)"}} mx={"auto"}>
             {children}
         </Box>
     </Flex>
@@ -31,3 +34,16 @@ const PageLayout = ({children}) => {
 }
 
 export default PageLayout;
+
+const PageLayoutSpinner = () => {
+    return (
+        <Flex 
+            flexDir={"column"}
+            h={"100vh"}
+            alignItems={"center"}
+            justifyContent={"center"}
+        >
+            <Spinner size={"xl"}/>
+        </Flex>
+    )
+};
--- src/hooks/useSignUpWithEmailAndPassword.js
+++ src/hooks/useSignUpWithEmailAndPassword.js
@@ -1,26 +1,32 @@
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { auth, firestore } from "../firebase/firebase";
-import { doc, setDoc } from 'firebase/firestore';
+import { collection, doc, getDocs, query, setDoc, where } from 'firebase/firestore';
 import useShowToast from './useShowToast';
 import useAuthStore from '../store/authStore';
 
 const useSignUpWithEmailAndPassword = () => {
 
-    const [
-        createUserWithEmailAndPassword,
-        loading,
-        error,
-      ] = useCreateUserWithEmailAndPassword(auth);
+    const [createUserWithEmailAndPassword, ,loading, error,] = useCreateUserWithEmailAndPassword(auth);
 
-      const showToast = useShowToast();
-      const loginUser= useAuthStore(state => state.login);
-    //   const logoutUser= userAuthStore(state => state.logout);
-      const signup = async (inputs) => {
+    const showToast = useShowToast();
+    const loginUser= useAuthStore((state) => state.login);
+
+    const signup = async (inputs) => {
 
         if(!inputs.email || !inputs.password || !inputs.fullName || !inputs.username){
             showToast("Error", "Please fill all the fields", 'error');
             return;
         }
+
+        const usersRef = collection(firestore, "users");
+
+		const q = query(usersRef, where("username", "==", inputs.username));
+		const querySnapshot = await getDocs(q);
+
+		if (!querySnapshot.empty) {
+			showToast("Error", "Username already exists", "error");
+			return;
+		}
 
         try {
             const newUser = await createUserWithEmailAndPassword(inputs.email, inputs.password)
@@ -48,9 +54,9 @@ const useSignUpWithEmailAndPassword = () => {
         } catch (error) {
             showToast("error", error.message, 'error');
         }
-      }
+    };
 
-  return {loading, error, signup}
+    return {loading, error, signup};
 }
 
-export default useSignUpWithEmailAndPassword
+export default useSignUpWithEmailAndPassword;
