0e9e31c9568ceefe6d3366176c3316fd03ffd023
Kriso068 <nicolas.gutknecht@numericable.fr>
2023-12-17 11:44:01 +0100

    feedPost

--- src/components/FeedPosts/FeedPosts.jsx
+++ src/components/FeedPosts/FeedPosts.jsx
@@ -29,7 +29,6 @@ const FeedPosts = () => {
 					<Text fontSize={"md"} color={"red.400"}>
 						Dayuum. Looks like you don&apos;t have any friends.
 					</Text>
-					<Text color={"red.400"}>Stop coding and go make some!!</Text>
 				</>
 			)}
 		</Container>
